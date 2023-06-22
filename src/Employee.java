5105fdebcdcc1e883ee8a4ea0fed155092a083cd
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-22 14:36:14 +0530

    Java Streams Fix

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,6 @@
 
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Employee {
     int id;
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 
 
 import java.sql.*;
-import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.Date;
 import java.util.stream.Collectors;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static Date getDate(String dateString) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public static void main(String[] args) throws SQLException {
         String jdbcUrl = "jdbc:mariadb://localhost:3306/terzo_employees";
@@ -46,120 +56,143 @@ public class Main {
             employeeList.add(tempEmp);
         }
 
+        List<Employee> empList = new ArrayList<>();
+
         //Exercise 1
-//        employeeList.stream()
-//                .filter(e -> e.name.startsWith("Y"))
-//                .forEach(System.out::println);
+        System.out.println("1. Filter the Employee with Name starts with \"D\"");
+        employeeList.stream()
+                .filter(e -> e.name.startsWith("Y"))
+                .forEach(System.out::println);
 
         //Exercise 2
-//        employeeList.stream()
-//                .filter(employee -> employee.mobileNo == null)
-//                .forEach(System.out::println);
+        System.out.println("2. Filter the Employee who did not have mobile number updated in DB\n");
+        employeeList.stream()
+                .filter(employee -> employee.mobileNo == null)
+                .forEach(System.out::println);
 
         //Exercise 3
-//        List<Employee> empList = new ArrayList<>();
-//        empList = employeeList.stream()
-//                .filter(employee -> employee.department.equals("QA") && employee.salary > 10000)
-//                .collect(Collectors.toList());
+        empList = employeeList.stream()
+                .filter(employee -> employee.department.equals("QA") && employee.salary > 10000)
+                .collect(Collectors.toList());
+        System.out.println("3. Obtain a list of employees belongs to category “QA” with salary > 10000" + empList);
 
 
         //Exercise 4
-//        List<Employee> empList = new ArrayList<>();
-//        empList = employeeList.stream()
-//                .filter(e -> e.department.equals("IT"))
-//                .collect(Collectors.toList());
+        empList = employeeList.stream()
+                .filter(e -> e.department.equals("IT"))
+                .collect(Collectors.toList());
+        System.out.println("4. Obtain a list of employees with products belong to department “IT”\n" + empList);
 
         //Exercise 5
-//        List<Employee> empList = new ArrayList<>();
-//        empList = employeeList.stream()
-//                .filter(employee -> employee.department.equals("DEV"))
-//                .map(employee -> new Employee(employee.id,employee.name,(int) (employee.salary+(0.1*employee.salary)),employee.department,employee.joiningDate,employee.mobileNo))
-//                .collect(Collectors.toList());
-
+        empList = employeeList.stream()
+                .filter(employee -> employee.department.equals("DEV"))
+                .map(employee -> new Employee(employee.id,employee.name,(int) (employee.salary+(0.1*employee.salary)),employee.department,employee.joiningDate,employee.mobileNo))
+                .collect(Collectors.toList());
+        System.out.println("5. Obtain a list of employees with department = “DEV” and then apply 10% increment in the salary" + empList);
+        employeeList.stream().close();
 
         //Exercise 6
-//        List<Employee> empList = new ArrayList<>();
-//        empList = employeeList.stream()
-//                .filter(employee -> employee.joiningDate.after(new java.util.Date(2021-02-01)) && employee.joiningDate.before(new java.util.Date("2021-04-01")) )
-//                .collect(Collectors.toList());
+        empList = employeeList.stream()
+                .filter(employee -> {
+                    Date startDate = getDate("2021-02-01");
+                    Date endDate = getDate("2021-04-01");
+                    Date joiningDate = employee.joiningDate;
+                    return joiningDate.after(startDate) && joiningDate.before(endDate);
+                })
+                .collect(Collectors.toList());
+        System.out.println("6. Obtain a list of employees joined between 01-Feb-2021 and 01-Apr-2021" + empList +"\n");
 
         //Exercise 7
+        System.out.println("7. Get the lowest salary of employee");
         employeeList.stream()
                 .mapToInt(Employee::getSalary)
                 .min()
                 .ifPresent(System.out::println);
 
         //Exercise 8
-//        employeeList.stream()
-//                .filter(employee -> employee.joiningDate.equals(new java.util.Date("2021-02-14")))
-//                .mapToInt(employee -> employee.salary)
-//                .average()
-//                .ifPresent(System.out::println);
+        System.out.println("8. Calculate average salary for employee joined on 14-Mar-2021");
+        Date date = getDate("2021-02-14");
+        employeeList.stream()
+                .filter(employee -> employee.joiningDate.equals(date))
+                .mapToInt(employee -> employee.salary)
+                .average()
+                .ifPresent(System.out::println);
 
         //Exercise 9
-//        List<Employee> empList = new ArrayList<>();
-//        empList = employeeList.stream()
-//                .sorted(Comparator.comparing(Employee::getJoiningDate)
-//                .reversed())
-//                .limit(3)
-//                .collect(Collectors.toList());
+        empList = employeeList.stream()
+                .sorted(Comparator.comparing(Employee::getJoiningDate)
+                .reversed())
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println("9. Get the 3 most recently joined\n" + empList+"\n");
 
         //Exercise 10
-//        employeeList.stream()
-//                .filter(employee -> employee.joiningDate.after(new java.util.Date("2021-01-31"))
-//                        && employee.joiningDate.before(new java.util.Date("2021-02-29")))
-//                .mapToInt(Employee::getSalary)
-//                .sum();
+        Date startDate = getDate("2021-01-31");
+        Date endDate = getDate("2021-02-29");
+        System.out.println("10. Calculate total  sum of all salary joined in Feb 2021\n" +
+                employeeList.stream()
+                .filter(employee -> employee.joiningDate.after(startDate)
+                        && employee.joiningDate.before(endDate))
+                .mapToInt(Employee::getSalary)
+                .sum()+"\n");
+
+
+
 
         //Exercise 11
-//        Map<String,Long> map = new HashMap<>();
-//        map = employeeList.stream()
-//                .collect(Collectors.groupingBy(Employee::getDepartment,Collectors.counting()));
+         Map<Integer,Long> map = new HashMap<>();
+        map = employeeList.stream().collect(Collectors.groupingBy(Employee::getSalary,Collectors.counting()));
+        System.out.println("11. Obtain a data map with  Salary and employees count" + map +"\n");
 
         //Exercise 12
-//        Map<String,List<Employee>> map = new HashMap<>();
-//        map = employeeList.stream()
-//                .collect(Collectors.groupingBy(Employee::getDepartment));
+        Map<String,List<Employee>> map4 = new HashMap<>();
+        map4 = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+        System.out.println("12. Produce a data map with employee records grouped by department"+map4+"\n");
 
 
         //Exercise 13
-//        Map<String,Integer> map = new HashMap<>();
-//        map = employeeList.stream()
-//                .collect(Collectors.groupingBy(Employee::getDepartment,Collectors.summingInt(Employee::getSalary)));
+        Map<String,Integer> map3 = new HashMap<>();
+        map3 = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,Collectors.summingInt(Employee::getSalary)));
+        System.out.println("13. Produce a data map with department and their salary\n"+map3+"\n");
+
 
         //Exercise 14
-//        Map<String,List<Employee>> map = new HashMap<>();
-//        map = employeeList.stream()
-//                .collect(Collectors.groupingBy(Employee::getDepartment));
-//
-//
-//        int maxSalary = Integer.MIN_VALUE;
-//        String dept = "";
-//        Iterator<Map.Entry<String, List<Employee>>> itr = map.entrySet().iterator();
-//
-//        while(itr.hasNext())
-//        {
-//            Map.Entry<String, List<Employee>> entry = itr.next();
-//            List<Employee> empList = entry.getValue();
-//            int temp = empList.stream()
-//                    .mapToInt(Employee::getSalary)
-//                    .sum();
-//            if(temp > maxSalary){
-//                maxSalary = temp;
-//                dept = entry.getKey();
-//            }
-//        }
+        System.out.println("14. Get the most highest paid by category\n");
+        Map<String,List<Employee>> map2 = new HashMap<>();
+        map2 = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+
+        int maxSalary = Integer.MIN_VALUE;
+        String dept = "";
+        Iterator<Map.Entry<String, List<Employee>>> itr = map2.entrySet().iterator();
+
+        while(itr.hasNext())
+        {
+            Map.Entry<String, List<Employee>> entry = itr.next();
+            List<Employee> empList1 = entry.getValue();
+            int temp = empList1.stream()
+                    .mapToInt(Employee::getSalary)
+                    .sum();
+            if(temp > maxSalary){
+                maxSalary = temp;
+                dept = entry.getKey();
+            }
+        }
+        System.out.println(dept+" "+maxSalary+"\n");
 
         //Exercise 15
-        Map<Integer,Long> map = new HashMap<>();
+        Map<Integer,Long> map1 = new HashMap<>();
 
-        map = employeeList.stream()
+        map1 = employeeList.stream()
                 .collect(Collectors.groupingBy(Employee::getSalary,Collectors.counting()));
 
-        Iterator<Map.Entry<Integer, Long>> itr = map.entrySet().iterator();
-        while (itr.hasNext())
+        Iterator<Map.Entry<Integer, Long>> itr1 = map1.entrySet().iterator();
+        System.out.println("Obtain a data map with  Salary and employees count\n");
+        while (itr1.hasNext())
         {
-           Map.Entry<Integer, Long> entry = itr.next();
+           Map.Entry<Integer, Long> entry = itr1.next();
            System.out.println(entry.getKey()+" "+entry.getValue());
         }
 
