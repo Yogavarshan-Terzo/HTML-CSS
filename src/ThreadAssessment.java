c013f6590e2a70afdeac24659b63842e8393877c
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-22 16:43:50 +0530

    Thread Assessment Ticket Booking

---
+++ src/ThreadAssessment.java
@@ -0,0 +1,45 @@
+class TrainServices{
+    int id = 0;
+    boolean bookingStarted = false;
+    public synchronized void bookTicket(){
+        while (bookingStarted){
+            try{wait();}catch (Exception e){}
+        }
+        bookingStarted = true;
+        System.out.println(Thread.currentThread().getName() + ": Ticket booked with id :" + id++);
+        notify();
+        bookingStarted = false;
+    }
+}
+class User implements Runnable{
+    String name;
+    int age;
+    TrainServices trainServices = new TrainServices();
+
+    public User(String name, int age, TrainServices trainServices) {
+        this.name = name;
+        this.age = age;
+        this.trainServices = trainServices;
+        Thread t = new Thread(this);
+        t.start();
+//        try {
+//            t.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    public void run(){
+        trainServices.bookTicket();
+    }
+
+}
+public class ThreadAssessment {
+    public static void main(String[] args) {
+        TrainServices trainServices = new TrainServices();
+        User person1 = new User("yoga",20,trainServices);
+        User person2 = new User("yuswanth",20,trainServices);
+        person1.run();
+    }
+
+}
