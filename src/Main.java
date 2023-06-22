9514cdb183a4c2fdaad6e884dacd2dd5d420645b
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-22 15:08:21 +0530

    Threads Join and isAlive

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Runnable obj1 = () -> {
             for(int i = 0; i < 10; i++){
                 System.out.println("Hi");
@@ -29,14 +29,25 @@ class Main{
         t1.start();
         t2.start();
 
+        System.out.println(t1.isAlive());
+
+        t1.join();
+        t2.join();
+
+        System.out.println(t1.isAlive());
+
+        System.out.println("bye");
+
+
+
     }
 }
-//class A implements Runnable{
+//class A{
 //    public void run(){
 //        for(int i = 0; i < 10; i++){
 //            System.out.println("Hi");
 //            try {
-//                Thread.sleep(10);
+//                Thread.sleep(1000);
 //            } catch (InterruptedException e) {
 //                throw new RuntimeException(e);
 //            }
