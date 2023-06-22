d7e6b70e86d9682cd2de9106da16caa8e4063d1e
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-22 12:54:49 +0530

    Threads sleep

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,11 @@ class A extends Thread{
     public void run(){
         for(int i = 0; i < 10; i++){
             System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -21,6 +26,11 @@ class B extends Thread{
     public void run(){
         for(int i = 0; i < 10; i++){
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
