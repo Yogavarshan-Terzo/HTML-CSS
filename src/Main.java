9b2cc65ba768b7dde1a94ac12c63a25bcb4a5f7c
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-22 15:24:18 +0530

    Threads Synchronized function

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,37 @@
-
+class Counter{
+    int count = 0;
+    public synchronized void increment(){
+        count++;
+    }
+}
 class Main{
 
     public static void main(String[] args) throws Exception {
+        Counter c = new Counter();
         Runnable obj1 = () -> {
-            for(int i = 0; i < 10; i++){
-                System.out.println("Hi");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            for(int i = 0; i < 1000; i++){
+                c.increment();
             }
         };
         Runnable obj2 = ()->{
-            for(int i = 0; i < 10; i++){
-                System.out.println("Hello");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            for(int i = 0; i < 1000; i++){
+                c.increment();
             }
         };
 
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
 
-        System.out.println(t1.getPriority());
-        System.out.println(t2.getPriority());
-
-        t1.setPriority(Thread.MIN_PRIORITY);
-        t2.setPriority(Thread.MAX_PRIORITY);
-
-        System.out.println(t1.getPriority());
-        System.out.println(t2.getPriority());
 
         t1.start();
         t2.start();
 
-        System.out.println(t1.isAlive());
 
         t1.join();
         t2.join();
 
-        System.out.println(t1.isAlive());
+        System.out.println(c.count);
 
-        System.out.println("bye");
 
 
 
